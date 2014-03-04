34a076893db06cd39288c5f968fcaeeb5576579c
Andrew Y / Jenna T <ayiu@pivotallabs.com>
2014-03-04 17:42:04 -0500

    Added ability to sign out

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ GiftsApplication::Application.routes.draw do
   root :to => 'static_pages#home'
   match '/signin', to: 'sessions#new', via: 'get', as: 'signin'
   match '/signup', to: 'users#new', via: 'get', as: 'signup'
-  match '/signout', to: 'sessions#destroy', via: 'delete', as: 'signout'
+  match '/signout', to: 'sessions#destroy', via: 'get', as: 'signout'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
